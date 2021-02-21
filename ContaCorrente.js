7d4f2f2f86bd2cd7af4e3041ca56117493ca5e5c
HcjunioR <hcjuniorxd@gmail.com>
2021-02-21 01:07:14 -0300

    Organização alterada

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -3,7 +3,8 @@ import { Cliente } from "./Cliente.js";
 export class ContaCorrente {
     agencia;
     _cliente;
-
+    _saldo = 0;// #saldo https://github.com/tc39/proposal-class-fields#private-fields
+    
     set cliente(novoValor) {
         if(novoValor instanceof Cliente){
             this._cliente = novoValor;
@@ -13,9 +14,6 @@ export class ContaCorrente {
     get cliente() {
         return this._cliente;
     }
-
-    // #saldo https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
 
     get saldo() {
         return this._saldo;
