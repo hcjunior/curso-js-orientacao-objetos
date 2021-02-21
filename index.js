764c8a8365538de20e396f4ca0e88dfdcb7e6c20
HcjunioR <hcjuniorxd@gmail.com>
2021-02-21 01:18:04 -0300

    Testes index.js otimizado

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const contaCorrenteRicardo = new ContaCorrente(1001,cliente1);
 
 contaCorrenteRicardo.depositar(500)
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(102,cliente2);
 
 contaCorrenteRicardo.transferir(200,conta2)
 
