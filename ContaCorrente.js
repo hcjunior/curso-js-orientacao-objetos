7d1b8a3af4b52343fe54b3a8322cd4dd40854dcb
HcjunioR <hcjuniorxd@gmail.com>
2021-02-21 03:21:48 -0300

    Contador adicionado + formatação ContaCorrente.js

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,12 +1,13 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
     _cliente;
     _saldo = 0;// #saldo https://github.com/tc39/proposal-class-fields#private-fields
-    
+
     set cliente(novoValor) {
-        if(novoValor instanceof Cliente){
+        if (novoValor instanceof Cliente) {
             this._cliente = novoValor;
         }
     }
@@ -19,26 +20,27 @@ export class ContaCorrente {
         return this._saldo;
     }
 
-    constructor(cliente, agencia) {
+    constructor(agencia, cliente) {
         this.agencia = agencia;
         this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
     }
 
     sacar(valor) {
-        if(this._saldo >= valor){
+        if (this._saldo >= valor) {
             this._saldo -= valor;
             return valor;
         }
     }
 
-    depositar(valor){
-        if(valor <= 0){
+    depositar(valor) {
+        if (valor <= 0) {
             return;
-        } 
-        this._saldo += valor;           
+        }
+        this._saldo += valor;
     }
 
-    transferir(valor, conta){
+    transferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
     }
