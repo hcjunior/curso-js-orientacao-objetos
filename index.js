2d211269831644aa9e4a721321bebf3b6183dd69
HcjunioR <hcjuniorxd@gmail.com>
2021-02-21 01:15:09 -0300

    Testes index.js otimizado

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo",11122233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+const cliente2 = new Cliente("Alice",88822233309);
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+const contaCorrenteRicardo = new ContaCorrente(1001,cliente1);
+
 contaCorrenteRicardo.depositar(500)
 
 const conta2 = new ContaCorrente();
